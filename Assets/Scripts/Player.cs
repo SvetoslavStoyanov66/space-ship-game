ce9b5308f58e6ed0d43dd62b16159f7ab0f9358e
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2023-07-27 17:03:28 +0300

    file update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,25 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _laserPrefab;
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         Movement();
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+        
+       
+        
     }
     void Movement()
     {
