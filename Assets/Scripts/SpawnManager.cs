077eab0331ec269c8f6a741972f435f8e309aadf
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2023-07-28 13:18:02 +0300

    random enemy spawning

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnDelay = 5.0f;
+    [SerializeField]
+    private GameObject prefabToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnRoutine()
+    {
+        int spawnx = Random.Range(-9, 9);
+        while (true)
+        {
+
+            Instantiate(prefabToSpawn, new Vector3(spawnx, 8, 0), Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
